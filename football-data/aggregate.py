5df812f61fd29ff95afcaa6a6e238fc7c416fa2c
csthunes <csthunes@ncsu.edu>
2023-04-03 20:14:27 -0400

    Aggregation for kickers and updating INJCOR calc.

--- football-data/aggregate.py
+++ football-data/aggregate.py
@@ -4,8 +4,9 @@ import numpy as np
 FPTS_tiers = [6, 9.5, 13.5, 18]
 HALF_tiers = [7, 10.75, 15.085, 20]
 PPR_tiers = [8, 12, 16.67, 22]
-te_tier_adj = -3
 qb_tier_adj = 6
+te_tier_adj = -3
+k_tier_adj = -2
 
 rb_FPTS_adj_constant = 1.615  # average of the 1/4 roots of fpts std dev
 rb_HALF_adj_constant = 1.633
@@ -17,6 +18,7 @@ te_FPTS_adj_constant = 1.430  # average of the 1/4 roots of fpts std dev
 te_HALF_adj_constant = 1.479
 te_PPR_adj_constant = 1.528
 qb_adj_constant = 1.637
+k_adj_constant = 1.249
 
 # weights for pts adjusted mean, pts median, quality start ratio, good start ratio, opportunities mean
 scoring_weights = [.45, .45, .5, .25, .1]
@@ -92,7 +94,8 @@ def agg_QBs(grouped):
     df.insert(21, "rush_Y/A", df["rush_YDS"] / df["rush_ATT"])
     df.insert(26, "OPP", df["pass_ATT"] + df["rush_ATT"])
     df.insert(27, "OPP_mean", df["OPP"] / df["G"])
-    df.insert(28, "INJCOR", np.where(df["G"] <= 8, np.log10(df["G"] + 1), 1))
+    df.insert(28, "INJCOR", np.where(
+        df["G"] <= 10, np.emath.logn(12, df["G"] + 1), 1))
 
     df.insert(33, "FPTS_adj", np.where(df["FPTS_std"] != 0, df["FPTS_mean"] / pow(
         df["FPTS_std"], 1/4) * qb_adj_constant, df["FPTS_mean"]))
@@ -198,7 +201,8 @@ def agg_RBs(grouped):
     df.insert(25, "OPP", df["rush_ATT"] + df["rec_TGT"])
     df.insert(26, "OPP_mean", df["OPP"] / df["G"])
     df.insert(29, "FL/T", df["FL"] / df["TOU"])
-    df.insert(30, "INJCOR", np.where(df["G"] <= 8, np.log10(df["G"] + 1), 1))
+    df.insert(30, "INJCOR", np.where(
+        df["G"] <= 10, np.emath.logn(12, df["G"] + 1), 1))
 
     df.insert(35, "FPTS_adj", np.where(df["FPTS_std"] != 0, df["FPTS_mean"] / pow(
         df["FPTS_std"], 1/4) * rb_FPTS_adj_constant, df["FPTS_mean"]))
@@ -304,7 +308,8 @@ def agg_WRs(grouped):
     df.insert(25, "OPP", df["rush_ATT"] + df["rec_TGT"])
     df.insert(26, "OPP_mean", df["OPP"] / df["G"])
     df.insert(29, "FL/T", df["FL"] / df["TOU"])
-    df.insert(30, "INJCOR", np.where(df["G"] <= 8, np.log10(df["G"] + 1), 1))
+    df.insert(30, "INJCOR", np.where(
+        df["G"] <= 10, np.emath.logn(12, df["G"] + 1), 1))
 
     df.insert(35, "FPTS_adj", np.where(df["FPTS_std"] != 0, df["FPTS_mean"] / pow(
         df["FPTS_std"], 1/4) * wr_FPTS_adj_constant, df["FPTS_mean"]))
@@ -410,7 +415,8 @@ def agg_TEs(grouped):
     df.insert(25, "OPP", df["rush_ATT"] + df["rec_TGT"])
     df.insert(26, "OPP_mean", df["OPP"] / df["G"])
     df.insert(29, "FL/T", df["FL"] / df["TOU"])
-    df.insert(30, "INJCOR", np.where(df["G"] <= 8, np.log10(df["G"] + 1), 1))
+    df.insert(30, "INJCOR", np.where(
+        df["G"] <= 10, np.emath.logn(12, df["G"] + 1), 1))
 
     df.insert(35, "FPTS_adj", np.where(df["FPTS_std"] != 0, df["FPTS_mean"] / pow(
         df["FPTS_std"], 1/4) * te_FPTS_adj_constant, df["FPTS_mean"]))
@@ -438,6 +444,110 @@ def agg_TEs(grouped):
                                 ) / np.where(df["PPR_bad"] != 0, df["PPR_bad"], 1))
     df.insert(69, "PPR_Score", df["INJCOR"] * (df["PPR_adj"] * scoring_weights[0] + df["PPR_median"]
               * scoring_weights[1] + df["PPR_QSRat"] * scoring_weights[2] + df["PPR_GSRat"] * scoring_weights[3] + df["OPP_mean"] * scoring_weights[4]))
+
+    df.replace(np.inf, 0)
+    df.fillna(0, inplace=True)
+    return df
+
+
+def agg_Ks(grouped):
+    df = grouped.agg(G=("Player", "size"),
+                     FG=("FG", "sum"),
+                     FG_mean=("FG", "mean"),
+                     FGA=("FGA", "sum"),
+                     FGA_mean=("FGA", "mean"),
+                     LG=("LG", "max"),
+                     LG_mean=("LG", "mean"),
+                     FG_10_19=("1-19", "sum"),
+                     FG_10_19_mean=("1-19", "mean"),
+                     FG_20_29=("20-29", "sum"),
+                     FG_20_29_mean=("20-29", "mean"),
+                     FG_30_39=("30-39", "sum"),
+                     FG_30_39_mean=("30-39", "mean"),
+                     FG_40_49=("40-49", "sum"),
+                     FG_40_49_mean=("40-49", "mean"),
+                     FG_50=("50+", "sum"),
+                     FG_50_mean=("50+", "mean"),
+                     XP=("XPT", "sum"),
+                     XP_mean=("XPT", "mean"),
+                     XPA=("XPA", "sum"),
+                     XPA_mean=("XPA", "mean"),
+                     FPTS=("FPTS", "sum"),
+                     FPTS_mean=("FPTS", "mean"),
+                     FPTS_median=("FPTS", "median"),
+                     FPTS_std=("FPTS", "std"),
+                     FPTS_bad=("FPTS", lambda x: sum(
+                         1 for y in x if y < FPTS_tiers[0] + k_tier_adj)),
+                     FPTS_poor=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[0] + k_tier_adj) and (y < FPTS_tiers[1] + k_tier_adj))),
+                     FPTS_okay=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[1] + k_tier_adj) and (y < FPTS_tiers[2] + k_tier_adj))),
+                     FPTS_good=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[2] + k_tier_adj) and (y < FPTS_tiers[3] + k_tier_adj))),
+                     FPTS_great=("FPTS", lambda x: sum(
+                         1 for y in x if y >= FPTS_tiers[3] + k_tier_adj)),
+                     HALF=("FPTS", "sum"),
+                     HALF_mean=("FPTS", "mean"),
+                     HALF_median=("FPTS", "median"),
+                     HALF_std=("FPTS", "std"),
+                     HALF_bad=("FPTS", lambda x: sum(
+                         1 for y in x if y < FPTS_tiers[0] + k_tier_adj)),
+                     HALF_poor=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[0] + k_tier_adj) and (y < FPTS_tiers[1] + k_tier_adj))),
+                     HALF_okay=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[1] + k_tier_adj) and (y < FPTS_tiers[2] + k_tier_adj))),
+                     HALF_good=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[2] + k_tier_adj) and (y < FPTS_tiers[3] + k_tier_adj))),
+                     HALF_great=("FPTS", lambda x: sum(
+                         1 for y in x if y >= FPTS_tiers[3] + k_tier_adj)),
+                     PPR=("FPTS", "sum"),
+                     PPR_mean=("FPTS", "mean"),
+                     PPR_median=("FPTS", "median"),
+                     PPR_std=("FPTS", "std"),
+                     PPR_bad=("FPTS", lambda x: sum(
+                         1 for y in x if y < FPTS_tiers[0] + k_tier_adj)),
+                     PPR_poor=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[0] + k_tier_adj) and (y < FPTS_tiers[1] + k_tier_adj))),
+                     PPR_okay=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[1] + k_tier_adj) and (y < FPTS_tiers[2] + k_tier_adj))),
+                     PPR_good=("FPTS", lambda x: sum(1 for y in x if (
+                         y >= FPTS_tiers[2] + k_tier_adj) and (y < FPTS_tiers[3] + k_tier_adj))),
+                     PPR_great=("FPTS", lambda x: sum(1 for y in x if y >= FPTS_tiers[3] + k_tier_adj)))
+
+    df.insert(1, "POS", "K")
+    df.insert(7, "FG_PCT", df["FG"] / df["FGA"])
+    df.insert(24, "XP_PCT", df["XP"] / df["XPA"])
+    df.insert(25, "OPP", 3 * df["FGA"] + df["XPA"])
+    df.insert(26, "OPP_mean", df["OPP"] / df["G"])
+    df.insert(27, "INJCOR", np.where(
+        df["G"] <= 10, np.emath.logn(12, df["G"] + 1), 1))
+
+    df.insert(32, "FPTS_adj", np.where(df["FPTS_std"] != 0, df["FPTS_mean"] / pow(
+        df["FPTS_std"], 1/4) * k_adj_constant, df["FPTS_mean"]))
+    df.insert(38, "FPTS_QSRat", (df["FPTS_great"] + df["FPTS_good"] + df["FPTS_okay"]) / np.where(
+        df["FPTS_poor"] + df["FPTS_bad"] != 0, df["FPTS_poor"] + df["FPTS_bad"], 1))
+    df.insert(39, "FPTS_GSRat", (df["FPTS_great"] + df["FPTS_good"]
+                                 ) / np.where(df["FPTS_bad"] != 0, df["FPTS_bad"], 1))
+    df.insert(40, "FPTS_Score", df["INJCOR"] * (df["FPTS_adj"] * scoring_weights[0] + df["FPTS_median"] * scoring_weights[1] +
+              df["FPTS_QSRat"] * scoring_weights[2] + df["FPTS_GSRat"] * scoring_weights[3] + df["OPP_mean"] * scoring_weights[4]))
+
+    df.insert(45, "HALF_adj", np.where(df["HALF_std"] != 0, df["HALF_mean"] / pow(
+        df["HALF_std"], 1/4) * k_adj_constant, df["HALF_mean"]))
+    df.insert(51, "HALF_QSRat", (df["HALF_great"] + df["HALF_good"] + df["HALF_okay"]) / np.where(
+        df["HALF_poor"] + df["HALF_bad"] != 0, df["HALF_poor"] + df["HALF_bad"], 1))
+    df.insert(52, "HALF_GSRat", (df["HALF_great"] + df["HALF_good"]
+                                 ) / np.where(df["HALF_bad"] != 0, df["HALF_bad"], 1))
+    df.insert(53, "HALF_Score", df["INJCOR"] * (df["HALF_adj"] * scoring_weights[0] + df["HALF_median"] * scoring_weights[1] +
+              df["HALF_QSRat"] * scoring_weights[2] + df["HALF_GSRat"] * scoring_weights[3] + df["OPP_mean"] * scoring_weights[4]))
+
+    df.insert(58, "PPR_adj", np.where(
+        df["PPR_std"] != 0, df["PPR_mean"] / pow(df["PPR_std"], 1/4) * k_adj_constant, df["PPR_mean"]))
+    df.insert(64, "PPR_QSRat", (df["PPR_great"] + df["PPR_good"] + df["PPR_okay"]) / np.where(
+        df["PPR_poor"] + df["PPR_bad"] != 0, df["PPR_poor"] + df["PPR_bad"], 1))
+    df.insert(65, "PPR_GSRat", (df["PPR_great"] + df["PPR_good"]
+                                ) / np.where(df["PPR_bad"] != 0, df["PPR_bad"], 1))
+    df.insert(66, "PPR_Score", df["INJCOR"] * (df["PPR_adj"] * scoring_weights[0] + df["PPR_median"] * scoring_weights[1] +
+              df["PPR_QSRat"] * scoring_weights[2] + df["PPR_GSRat"] * scoring_weights[3] + df["OPP_mean"] * scoring_weights[4]))
 
     df.replace(np.inf, 0)
     df.fillna(0, inplace=True)
--- football-data/wrangle.py
+++ football-data/wrangle.py
@@ -3,25 +3,28 @@ from aggregate import *
 
 currentYear = 2023
 # positions = ["qb", "rb", "wr", "te", "k", "dst"]
-positions = ["qb"]
+positions = ["qb", "rb", "wr", "te", "k"]
 # each year is weighed 2.5x more than the previous year (~62%, ~24%, ~10%, ~4%)
 weights = [15.625, 6.25, 2.5, 1]
 
 
 def wrangle(pos, year):
-    header = 0
-    if pos == "qb" or pos == "rb" or pos == "wr" or pos == "te":
-        header = 1
+    header = 1
+    if pos == "k" or pos == "dst":
+        header = 0
     df = pd.read_csv(
         "football-data/raw/{}/{}.csv".format(pos, year), header=header)
     df = df.sort_values(["Player", "Week"])
+
     if pos == "rb" or pos == "wr" or pos == "te":
         df["HALF"] = df["FPTS"] + df["REC"] * 0.5
         df["PPR"] = df["FPTS"] + df["REC"]
     else:
         df["HALF"] = df["FPTS"]
         df["PPR"] = df["FPTS"]
+
     grouped = df.groupby("Player", sort=False, as_index=False)
+
     if pos == "rb":
         df = agg_RBs(grouped)
     elif pos == "wr":
@@ -30,6 +33,8 @@ def wrangle(pos, year):
         df = agg_TEs(grouped)
     elif pos == "qb":
         df = agg_QBs(grouped)
+    elif pos == "k":
+        df = agg_Ks(grouped)
     else:
         df = grouped.agg(Pos=("Player", lambda x: pos.upper()),
                          G=("Player", "size"),
@@ -42,9 +47,11 @@ def wrangle(pos, year):
 
 def wrangle_all(all, pos):
     grouped = all.groupby("Player", sort=False, as_index=False)
+
     # average every column over all years for each player using list of weights
     all = grouped.apply(lambda x, cols: pd.Series(np.average(
         x[cols], weights=x["Weight"], axis=0), cols), list(all.columns.values)[2:-1])
+
     # include sum of weights and YOE based min weight present
     year = grouped.agg(Weight_sum=("Weight", "sum"), YOE=(
         "Weight", lambda x: weights.index(x.min()) + 1))
@@ -53,10 +60,12 @@ def wrangle_all(all, pos):
     all.insert(1, "TEAM", "")
     all[["Player", "TEAM"]] = all.Player.str.split("(", expand=True)
     all["TEAM"] = all.TEAM.str.replace(')', '', regex=False)
+
     # must have played in the last 2 seasons
     all = all[all["Weight_sum"] >= weights[1]]
     all = all[np.logical_not(
         (all["Weight_sum"] < weights[0]) & (all["TEAM"] == "FA"))]
+
     # add column for role on team (ranking on team for position)
     all.insert(3, "DPCHT", all.groupby("TEAM")[
                "OPP_mean"].transform("rank", ascending=False))
